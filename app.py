d5e19a4bf6f1427746971e908440b74c60c57ef9
stanpe <steffen.a.pettersen@student.uib.no>
2025-05-28 09:44:40 +0200

    Fixing links

--- app.py
+++ app.py
@@ -146,5 +146,6 @@ if st.button("Draw!"):
         for i, player in enumerate(results):
             st.subheader(f"Player {i+1}")
             df = pd.DataFrame(player, columns=["Name", "ID", "Expansion", "Link", "Traits"])
-            df["Link"] = df["Link"].apply(lambda url: f"[🔗]({url})")
-            st.dataframe(df)
+            df["Link"] = df["Link"].apply(lambda url: f"[🔗 Link]({url})")
+            st.markdown(df.to_markdown(index=False), unsafe_allow_html=True)
+
